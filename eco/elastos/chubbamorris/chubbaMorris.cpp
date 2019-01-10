5cf71458d2c4cd63273d9bb7724007eb45ec69a8
John L Olsen <john@itcsolutions.com.au>
2019-01-11 06:17:01 +1100

    Add files via upload

---
+++ eco/elastos/chubbamorris/chubbaMorris.cpp
@@ -0,0 +1,52 @@
+#include "elastos/chubbamorris/CProperty.h"
+
+#include "elastos/chubbamorris/CCustomer.h"
+
+#include "elastos/chubbamorris/CSolicitor.h"
+
+#include "elastos/chubbamorris/CSupplier.h"
+
+#include "elastos/chubbamorris/CAdministration.h"
+
+#include "elastos/chubbamorris/CFinance.h"
+
+#include "elastos/chubbamorris/CFactory.h"
+
+#include "elastos/chubbamorris/CStakeholder.h"
+
+#include "elastos/chubbamorris/CRegulator.h"
+
+#include "elastos/chubbamorris/CHelper.h"
+
+#include "elastos/chubbamorris/CRegister.h"
+
+
+namespace Elastos {
+
+namespace ChubbaMorris {
+
+CAR_OBJECT_IMPL(CProperty.h)
+
+CAR_OBJECT_IMPL(CCustomer.h)
+
+CAR_OBJECT_IMPL(CSolicitor.h)
+
+CAR_OBJECT_IMPL(CSupplier.h)
+
+CAR_OBJECT_IMPL(CAdministration.h)
+
+CAR_OBJECT_IMPL(CFinance.h)
+
+CAR_OBJECT_IMPL(CFactory.h)
+
+CAR_OBJECT_IMPL(CStakeholder.h)
+
+CAR_OBJECT_IMPL(CRegulator.h)
+
+CAR_OBJECT_IMPL(CHelper.h)
+
+CAR_OBJECT_IMPL(CRegister.h)
+
+} // ChubbaMorris
+
+} // Elastos
